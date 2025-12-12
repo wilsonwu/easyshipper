e40a58bc08c72decd8538592c1b6dde815bc9493
Wilson Wu <iwilsonwu@gmail.com>
2025-12-12 15:25:39 +0800

    Implement localization and enhance popup functionality

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 console.log('This is a popup!');
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('title').innerText = chrome.i18n.getMessage("popupTitle");
+});
